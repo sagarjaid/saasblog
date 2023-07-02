b62065f1ae299240612505096fe89c423fb9f03d
sagarjaid <sagarjaid321@gmail.com>
2023-07-03 02:04:21 +0530

    MVP v1 Live

---
+++ pages/done.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Done = () => {
+  return (
+    <div className='flex flex-col justify-center items-center h-screen gap-6'>
+      <div className='text-5xl font-bold'>Thank you!</div>
+      <div>I'm #bulidinpublic, help me spread the word</div>
+      <div className='flex items-center gap-1 justify-around rounded-full bg-green-600 p-1.5 px-4 text-white'>
+        <a
+          href='https://twitter.com/jaid_sagar'
+          target="_blank"
+          className='text-sm'>
+          Follow us on Twitter
+        </a>
+        <svg
+          className='w-5'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth={1.5}
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
+          aria-hidden='true'>
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75'
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Done;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center bg-gradient-to-t from-slate-100 via-slate-100 to-green-400 h-screen overflow-hidden'>
+    <div className='flex flex-col items-center bg-gradient-to-t from-slate-100 via-slate-100 to-green-200 h-screen overflow-hidden'>
       <main className="flex max-w-5xl gap-6 items-center flex-col w-full">
         <nav className="flex w-full items-center justify-between px-4 py-5">
-          <img src="/logo.png" className="w-[120px]" />
+          <img src="/saaslogo.png" className="w-[120px]" />
           <div className="flex items-center gap-1 justify-around rounded-full bg-black p-1.5 px-4 text-white">
-            <a href="/" className='text-sm' >get early access</a>
+            <a href="https://tally.so/r/mVj71v" className='text-sm' >get early access</a>
             <svg
               className="w-5"
               fill="none"
@@ -30,7 +30,7 @@ const Home = () => {
           <div>
             Simple & Modern WordPress
           </div>
-          <div>  blog Alternative for</div>
+          <div>blog Alternative for</div>
           <div className="text-rose-600">
             <Typewriter
               options={{
@@ -46,9 +46,9 @@ const Home = () => {
             />
           </div>
         </div>
-        <p className="text-center text-xs sm:text-base">Create a blog in 30 seconds with AI SEO!</p>
+        <p className="text-center text-xs sm:text-base">Create a blog in 30 seconds</p>
         <div className="flex w-fit items-center justify-center rounded-full bg-rose-600 p-2 px-4	text-white shadow-xl hover:bg-rose-700 ">
-          <a href="/ai-tools">get early access</a>
+          <a href="https://tally.so/r/mVj71v">get early access</a>
           <svg
             className="w-6"
             fill="none"
