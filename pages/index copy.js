ff05f39514da88803f804e82c4aefbb97fb343f7
sagarjaid <sagarjaid321@gmail.com>
2023-07-02 20:48:50 +0530

    First setup

--- pages/index copy.js
+++ pages/index copy.js
@@ -3,9 +3,9 @@ import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center bg-gradient-to-t from-slate-100 via-slate-100 to-green-400 h-screen overflow-hidden'>
-      <main className="flex max-w-5xl gap-6 items-center flex-col w-full">
-        <nav className="flex w-full items-center justify-between px-4 py-5">
+    <div className='bg-gradient-to-t text-center from-slate-100 via-slate-100 to-green-500'>
+      <main className="m-auto flex max-w-5xl gap-6 items-center flex-col">
+        <nav className="flex w-full items-center justify-between p-4">
           <img src="/logo.png" className="w-[120px]" />
           <div className="flex items-center gap-1 justify-around rounded-full bg-black p-1.5 px-4 text-white">
             <a href="/" className='text-sm' >get early access</a>
@@ -26,10 +26,11 @@ const Home = () => {
             </svg>
           </div>
         </nav>
-        <div className='flex flex-col items-center pt-16 text-base font-extrabold xs:text-2xl sdm:text-3xl sm:gap-3 md:text-4xl mdx:text-5xl '>
+        <div className='flex flex-col items-center pt-20 text-base font-extrabold xs:text-2xl sdm:text-3xl sm:gap-3 md:text-4xl mdx:text-5xl '>
           <div>
             Simple & Modern WordPress
           </div>
+          {/* <div className='flex gap-3'> */}
           <div>  blog Alternative for</div>
           <div className="text-rose-600">
             <Typewriter
@@ -45,8 +46,9 @@ const Home = () => {
               }}
             />
           </div>
+          {/* </div> */}
         </div>
-        <p className="text-center text-xs sm:text-base">Create a blog in 30 seconds with AI SEO!</p>
+        <p className="text-center text-xs sm:text-base">Create a blog in 30 sec*</p>
         <div className="flex w-fit items-center justify-center rounded-full bg-rose-600 p-2 px-4	text-white shadow-xl hover:bg-rose-700 ">
           <a href="/ai-tools">get early access</a>
           <svg
@@ -69,8 +71,9 @@ const Home = () => {
           <span className='text-xs'>✓ No credit card required</span>
           <span className='text-xs'>✓ No login is required</span>
         </div>
+        <img src="/banner.gif" className="object-cover" />
       </main>
-      <img src="/single-hero.svg" className="pt-8 object-cover w-full max-w-5xl" />
+      {/* <img src="/hero-2.png" className="object-cover" /> */}
     </div>
   )
 }
