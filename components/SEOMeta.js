9905bcfab9aa9c456e0244fe33bd4e35e2988ea0
sagarjaid <sagarjaid321@gmail.com>
2023-07-03 06:39:23 +0530

    MVP v2 Live

---
+++ components/SEOMeta.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+
+const SEOMeta = (props) => {
+  const { title, description, slug, imgUrl } = props;
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <link rel="canonical" href={`https://saasblog.ai/`} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`https://saasblog.ai/`} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta
+        property="og:image"
+        content={imgUrl || 'https://saasblog.ai/cover.png'}
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={`https://saasblog.ai/`} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta
+        property="twitter:image"
+        content={'https://saasblog.ai/cover.png'}
+      />
+      <link rel="icon" href="https://saasblog.ai/favicon.png" />
+
+    </Head>
+  );
+};
+
+export default SEOMeta;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,62 @@
+import SEOMeta from '@/components/SEOMeta';
 import React from 'react'
 import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center bg-gradient-to-t from-slate-100 via-slate-100 to-green-200 h-screen overflow-hidden'>
-      <main className="flex max-w-5xl gap-6 items-center flex-col w-full">
-        <nav className="flex w-full items-center justify-between px-4 py-5">
-          <img src="/saaslogo.png" className="w-[120px]" />
-          <div className="flex items-center gap-1 justify-around rounded-full bg-black p-1.5 px-4 text-white">
-            <a href="https://tally.so/r/mVj71v" className='text-sm' >get early access</a>
+    <>
+      <SEOMeta
+        title="SaaSblog - Simple & Modern blog for your SaaS/Business"
+        description="A WordPress blog Alternative for SaaS Startups, Indie hackers, Businesses and Bloggers"
+      />
+      <div className='flex flex-col items-center bg-gradient-to-t from-slate-100 via-slate-100 to-green-200 h-screen overflow-hidden'>
+        <main className="flex max-w-5xl gap-6 items-center flex-col w-full">
+          <nav className="flex w-full items-center justify-between px-4 py-5">
+            <img src="/saaslogo.png" className="w-[120px]" />
+            <div className="flex items-center gap-1 justify-around rounded-full bg-black p-1.5 px-4 text-white">
+              <a href="https://tally.so/r/mVj71v" className='text-sm' >get early access</a>
+              <svg
+                className="w-5"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={1.5}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                />
+              </svg>
+            </div>
+          </nav>
+          <div className='flex flex-col items-center pt-16 text-base font-extrabold xs:text-2xl sdm:text-3xl sm:gap-3 md:text-4xl mdx:text-5xl '>
+            <div>
+              Simple & Modern WordPress
+            </div>
+            <div>blog Alternative for</div>
+            <div className="text-rose-600">
+              <Typewriter
+                options={{
+                  strings: [
+                    'Indie Hackers',
+                    'SaaS Startups',
+                    "Bloggers",
+                    "Business"
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </div>
+          </div>
+          <p className="text-center text-xs sm:text-base">Create a blog in 30 seconds</p>
+          <div className="flex w-fit items-center justify-center rounded-full bg-rose-600 p-2 px-4	text-white shadow-xl hover:bg-rose-700 ">
+            <a href="https://tally.so/r/mVj71v">get early access</a>
             <svg
-              className="w-5"
+              className="w-6"
               fill="none"
               stroke="currentColor"
               strokeWidth={1.5}
@@ -25,53 +71,14 @@ const Home = () => {
               />
             </svg>
           </div>
-        </nav>
-        <div className='flex flex-col items-center pt-16 text-base font-extrabold xs:text-2xl sdm:text-3xl sm:gap-3 md:text-4xl mdx:text-5xl '>
-          <div>
-            Simple & Modern WordPress
+          <div className="flex flex-col items-center gap-1 text-center  text-gray-600 sm:text-base">
+            <span className='text-xs'>✓ No credit card required</span>
+            <span className='text-xs'>✓ No login is required</span>
           </div>
-          <div>blog Alternative for</div>
-          <div className="text-rose-600">
-            <Typewriter
-              options={{
-                strings: [
-                  'Indie Hackers',
-                  'SaaS Startups',
-                  "Bloggers",
-                  "Business"
-                ],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </div>
-        </div>
-        <p className="text-center text-xs sm:text-base">Create a blog in 30 seconds</p>
-        <div className="flex w-fit items-center justify-center rounded-full bg-rose-600 p-2 px-4	text-white shadow-xl hover:bg-rose-700 ">
-          <a href="https://tally.so/r/mVj71v">get early access</a>
-          <svg
-            className="w-6"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={1.5}
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-            />
-          </svg>
-        </div>
-        <div className="flex flex-col items-center gap-1 text-center  text-gray-600 sm:text-base">
-          <span className='text-xs'>✓ No credit card required</span>
-          <span className='text-xs'>✓ No login is required</span>
-        </div>
-      </main>
-      <img src="/single-hero.svg" className="pt-8 object-cover w-full max-w-5xl" />
-    </div>
+        </main>
+        <img src="/single-hero.svg" className="pt-8 object-cover w-full max-w-5xl" />
+      </div>
+    </>
   )
 }
 
