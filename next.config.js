7b09cb9c77674fe733c2651506c29dcddac3b883
sagarjaid <sagarjaid321@gmail.com>
2023-07-19 13:16:11 +0530

    POC API 3

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GT: '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',
-    VT: 'fvD4zbpsmp2QurqraGFmbyCe'
-  },
 }
 
 module.exports = nextConfig
