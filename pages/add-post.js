a6dc5bd0726be29f09885cc406618c16232dbe1d
sagarjaid <69151227+sagarjaid@users.noreply.github.com>
2023-08-04 18:35:07 +0000

    add post modified

--- pages/add-post.js
+++ pages/add-post.js
@@ -24,178 +24,40 @@ const AddPost = () => {
             <div className=" w-full flex flex-col p-4 border gap-2 rounded-lg ">
               <div className="flex text-xs justify-between">
 
-              <div className="flex w-full flex-col gap-1 text-sm">
-                <input
-                  onChange={""}
-                  value={""}
-                  placeholder="Post Title"
-                  type="text"
-                  className="border w-full border-gray-300 rounded-lg p-2"
-                />
-              </div>
-              </div>
-              
-
-              <div className="h-[520px] overflow-y-scroll">
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">Uncategorized</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/uncategorized</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">Saas</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/micro-saas</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
-                </div>
-
-                <div className="flex justify-between border-t cursor-pointer hover:bg-stone-50 py-4 px-1  items-center">
-                  <div className="font-bold ">New</div>
-                  <div className=" w-[150px] flex justify-start">
-                    <div className=" w-max text-xs">/new</div>
-                  </div>
+                <div className="flex w-full flex-col gap-1 text-sm">
+                  <input
+                    onChange={""}
+                    value={""}
+                    placeholder="Post Title"
+                    type="text"
+                    className="border w-full border-gray-300 rounded-lg p-2"
+                  />
                 </div>
               </div>
-            </div>
 
-            <div className="flex flex-col gap-6 w-2/5 p-4 border rounded-lg">
-              <label className="font-bold">Add New Category</label>
-
-              <div className="flex flex-col gap-1 text-sm">
-                <label className="font-bold">Name</label>
-                <input
-                  onChange={""}
-                  value={""}
-                  type="text"
-                  className="border border-gray-300 rounded-lg p-2"
-                />
-              </div>
-
-              <div className="flex flex-col gap-1 text-sm">
-                <label className="font-bold">Slug URL</label>
-                <input
-                  onChange={""}
-                  value={""}
-                  type="text"
-                  className="border border-gray-300 rounded-lg p-2"
-                />
-              </div>
-
-              <div className="flex flex-col gap-1 text-sm">
-                <label className="font-bold">SEO Title</label>
-                <input
-                  onChange={""}
-                  value={""}
-                  type="text"
-                  className="border border-gray-300 rounded-lg p-2"
-                />
-              </div>
 
               <div className="flex flex-col gap-1 text-sm">
                 <label className="font-bold">SEO Description</label>
-
                 <textarea
-                  rows="6"
+                  // rows="6"
                   value={""}
-                  className="border border-gray-300 rounded-lg p-2"
+                  className="border h-[50vh] border-gray-300 rounded-lg p-2"
                 />
               </div>
+            </div>
 
-              <div className="border bg-gray-900 w-fit text-white cursor-pointer border-gray-900 text-center p-1 px-2 rounded-md">
-                Add Category
+            <div className="w-2/5 ">
+              <div className="flex flex-col gap-6 p-4 border rounded-lg">
+                <label className="font-bold">Add New Category</label>
+                <div className="flex flex-col gap-1 text-sm">
+                  <label className="font-bold">Name</label>
+                  <input
+                    onChange={""}
+                    value={""}
+                    type="text"
+                    className="border border-gray-300 rounded-lg p-2"
+                  />
+                </div>
               </div>
             </div>
           </div>
